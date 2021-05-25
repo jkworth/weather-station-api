be9f75209eeff0636e095c5db6ae3068ac091473
Jeff Killingsworth <jkillingsworth@ic-1.com>
2021-05-25 16:15:02 -0400

    added ssl to api

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { readFileSync } from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const httpsOptions = {
+    key: readFileSync('./key.pem'),
+    cert: readFileSync('./cert.pem'),
+  };
+
   const app = await NestFactory.create(AppModule, {
-    // logger: ['error', 'warn'],
+    cors: {
+      origin: ['http://localhost:4200', 'https://weather-station-ui.web.app'],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    },
+    httpsOptions,
   });
-  await app.listen(3000);
+  await app.listen(3443, '0.0.0.0');
 }
 bootstrap();
