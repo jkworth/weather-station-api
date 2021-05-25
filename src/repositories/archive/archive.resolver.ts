94a70c35e9b4a6c272c556b800f782c209e41281
Jeff Killingsworth <jkillingsworth@ic-1.com>
2021-05-25 08:43:51 -0400

    updated archive to match rest of the repos

--- src/repositories/archive/archive.resolver.ts
+++ src/repositories/archive/archive.resolver.ts
@@ -1,6 +1,4 @@
-import { Args, Query, Resolver, Subscription } from '@nestjs/graphql';
-import { ShapeWherePipe } from 'src/repositories/common/pipes/shape-where.pipe';
-import { ArchiveWhereArgs } from './archive-where.args';
+import { Query, Resolver, Subscription } from '@nestjs/graphql';
 import { Archive } from './archive.entity';
 import { ArchiveService } from './archive.service';
 
@@ -8,12 +6,9 @@ import { ArchiveService } from './archive.service';
 export class ArchiveResolver {
   constructor(private service: ArchiveService) {}
 
-  @Query(() => [Archive], { name: 'archives' })
-  async getArchives(
-    @Args('where', { nullable: true, type: () => [ArchiveWhereArgs] }, ShapeWherePipe)
-    where?: ArchiveWhereArgs[],
-  ): Promise<Archive[]> {
-    return this.service.findAll(where);
+  @Query(() => [Archive])
+  async archiveForLast24Hours(): Promise<Archive[]> {
+    return this.service.findAllForLast24Hours();
   }
 
   @Subscription(() => Archive)
